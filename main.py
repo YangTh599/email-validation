83d824b5e9ee621504ec2e8fbb1833d135421e6d
YangTh599 <YangTh599@northwested.org>
2024-12-09 10:05:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
-# Student name
-# Current date
+# Thayer Yang
+# 09 DEC 2024
 # Email Validation in Python
 
+def validate_username(username):
+    if username.isalpha():
+        return "Username is valid (letters only)!"
+    elif username.isdigit():
+        return "Username is valid (digits only)!"
+    else:
+         return "Invalid username!"
+
+run = True
+while run:    
+    user = input("Enter a username: ")
+    if user == "quit PLEASE":
+        run = False
+    else:
+        print(validate_username(user))
+        print()
